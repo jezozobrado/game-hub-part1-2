ce8f1d6b28f59a2cbfde95a8af09fba4c0bca8ae
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-25 16:49:59 +0800

    Build responsive layout.

--- game-hub-speaknow/src/App.tsx
+++ game-hub-speaknow/src/App.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Submit</Button>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        gridTemplateColumns={"150px 1fr"}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
--- game-hub-speaknow/src/main.tsx
+++ game-hub-speaknow/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import "./index.css";
 import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
