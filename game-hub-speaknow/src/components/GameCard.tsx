0959a71100cb05e4223ccfb5269905c65ae5bbc9
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-25 23:56:15 +0800

    Revert "Change aspect ratio of images to 16/9."
    This reverts commit a5a3faa617fe67caea55837873a3e5c3a3092092.

--- game-hub-speaknow/src/components/GameCard.tsx
+++ game-hub-speaknow/src/components/GameCard.tsx
@@ -1,12 +1,4 @@
-import {
-  AspectRatio,
-  Box,
-  Card,
-  CardBody,
-  Heading,
-  Image,
-  Link,
-} from "@chakra-ui/react";
+import { Box, Card, CardBody, Heading, Image, Link } from "@chakra-ui/react";
 import Game from "../entities/Game";
 import CriticScore from "./CriticScore";
 import useGame from "../hooks/useGame";
@@ -21,18 +13,9 @@ const GameCard = ({ game }: Props) => {
 
   return (
     <>
-      <Card
-        borderRadius={10}
-        overflow="hidden"
-        _hover={{
-          transform: "scale(1.03)",
-          transition: "transform 0.15s ease-in",
-        }}
-      >
+      <Card borderRadius={10} overflow="hidden">
         <CardBody padding={0}>
-          <AspectRatio ratio={16 / 9}>
-            <Image src={game.background_image} objectFit="cover" />
-          </AspectRatio>
+          <Image src={game.background_image} />
           <Box padding={4}>
             <Link href={data?.website}>
               <Heading fontSize="2xl">{game.name}</Heading>
