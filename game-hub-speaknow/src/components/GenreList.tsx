a3b924417b89c0cdb749d7dca929d3aa268ca789
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-26 21:54:22 +0800

    Try bidirectional inifnite query. Almost succeed.

--- game-hub-speaknow/src/components/GenreList.tsx
+++ game-hub-speaknow/src/components/GenreList.tsx
@@ -1,8 +1,28 @@
-import { HStack, Heading, Image, List, ListItem } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import React from "react";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const pageSize = 12;
+  const {
+    data: genres,
+    fetchNextPage,
+    fetchPreviousPage,
+  } = useGenres(pageSize);
+
+  // console.log(hasNextPage, hasPreviousPage, genres);
+  // console.log((genres?.pages)!.length);
+  console.log(genres?.pages, genres?.pages.at(-1)?.next);
 
   return (
     <>
@@ -10,6 +30,22 @@ const GenreList = () => {
         Genres
       </Heading>
       <List spacing={2}>
+        {genres?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((genre) => (
+              <HStack key={genre.id}>
+                <Image
+                  src={genre.image_background}
+                  boxSize="35px"
+                  borderRadius={5}
+                />
+                <ListItem fontSize="lg">{genre.name}</ListItem>
+              </HStack>
+            ))}
+          </React.Fragment>
+        ))}
+      </List>
+      {/* 
         {genres?.results.map((genre) => (
           <HStack key={genre.id}>
             <Image
@@ -19,8 +55,23 @@ const GenreList = () => {
             />
             <ListItem fontSize="lg">{genre.name}</ListItem>
           </HStack>
-        ))}
-      </List>
+        ))} */}
+
+      {genres?.pages.at(-1)?.next ? (
+        <Button marginTop={4} onClick={() => fetchNextPage()}>
+          <HStack>
+            <Icon as={BsChevronDown} />
+            <Text>Show more</Text>
+          </HStack>
+        </Button>
+      ) : (
+        <Button marginTop={4} onClick={() => fetchPreviousPage()}>
+          <HStack>
+            <Icon as={BsChevronUp} />
+            <Text>Hide</Text>
+          </HStack>
+        </Button>
+      )}
     </>
   );
 };
--- game-hub-speaknow/src/hooks/useGenres.ts
+++ game-hub-speaknow/src/hooks/useGenres.ts
@@ -1,13 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/apiClient";
 import { Genre } from "../entities/Genre";
 
-const apiClient = new APIClient<Genre>("/genres");
-const useGenres = () =>
-  useQuery<FetchResponse<Genre>>({
-    queryKey: ["genres"],
-    queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
-  });
+export interface GenreQuery {
+  page?: number;
+  pageSize: number;
+}
 
+const apiClient = new APIClient<Genre>("/genres");
+const useGenres = (pageSize: number) => {
+  return useInfiniteQuery<FetchResponse<Genre>>({
+    queryKey: ["genres", pageSize],
+    queryFn: ({ pageParam }) => {
+      console.log(pageParam);
+
+      return apiClient.getAll({ page: pageParam, pageSize });
+    },
+    staleTime: 24 * 60 * 60 * 1000,
+    keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => allPages.length + 1,
+    getPreviousPageParam: (firstPage, allPages) => 1,
+  });
+};
 export default useGenres;
