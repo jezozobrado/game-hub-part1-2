d5e2453afbb4e15c936182b5eb39e27b54aa1365
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-28 02:08:06 +0800

    Make the logo clickable.

--- game-hub-speaknow/src/components/NavBar.tsx
+++ game-hub-speaknow/src/components/NavBar.tsx
@@ -1,12 +1,15 @@
 import { HStack, Image, Switch, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack marginX={2}>
-      <Image src={logo} boxSize="60px" />
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <Switch
         colorScheme="green"
