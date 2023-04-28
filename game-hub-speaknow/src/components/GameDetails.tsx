31d9bad0b4db6c89a1d4984aa24c24214d158da0
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-28 17:58:29 +0800

    Implement link tree.

--- game-hub-speaknow/src/components/GameDetails.tsx
+++ game-hub-speaknow/src/components/GameDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "./ExpandableText";
 import { Badge, Box, HStack, Heading, Text } from "@chakra-ui/react";
@@ -13,23 +13,38 @@ const GameDetails = () => {
     dateReleased.indexOf(" ") + 1
   );
 
+  if (!game) return null;
+
   return (
-    <Box>
-      <HStack fontSize="16px" spacing={5}>
-        <Badge
-          borderRadius={5}
-          bgColor="white"
-          color="gray.800"
-          fontSize="16px"
-        >
-          {dateReleasedWithoutDay}
-        </Badge>
-        <PlatformList gamePlatforms={game?.parent_platforms} />
-        <Text>{`AVERAGE PLAYTIME: ${game?.playtime} HOURS`}</Text>
-      </HStack>
-      <Heading fontSize="70px">{game?.name}</Heading>
-      <ExpandableText children={game?.description_raw} />
-    </Box>
+    <>
+      <Box>
+        <HStack marginY={6} color="gray.500" fontSize="sm">
+          <Link to="/">
+            <Text
+              _hover={{ color: "white", transition: "color 0.15s ease-in" }}
+            >
+              HOME
+            </Text>
+          </Link>
+          <Text>/</Text>
+          <Text>{game.name!.toUpperCase()}</Text>
+        </HStack>
+        <HStack fontSize="16px" spacing={5}>
+          <Badge
+            borderRadius={5}
+            bgColor="white"
+            color="gray.800"
+            fontSize="16px"
+          >
+            {dateReleasedWithoutDay}
+          </Badge>
+          <PlatformList gamePlatforms={game.parent_platforms} />
+          <Text>{`AVERAGE PLAYTIME: ${game.playtime} HOURS`}</Text>
+        </HStack>
+        <Heading fontSize="70px">{game.name}</Heading>
+        <ExpandableText children={game.description_raw} />
+      </Box>
+    </>
   );
 };
 
